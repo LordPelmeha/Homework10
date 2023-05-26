4ff303ed5553fbd60687d1d6bab9d7e08ebbd903
Yamat0Damashii <maksim.orlach@yandex.ru>
2023-05-26 21:14:28 +0300

    Часть С сделана :(

---
+++ Homework10/Formula.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Homework10
+{
+    public class Formula
+    {
+        public string _fname;
+        public string _expr;
+        
+
+        public Formula(string fname, string expr)
+        {
+            this._fname = fname;
+            this._expr = expr;
+        }
+
+    }
+}
+
--- Homework10/Program.cs
+++ Homework10/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace Homework10
+﻿using System.Text.RegularExpressions;
+
+namespace Homework10
 {
     internal class Program
     {
         static void Main()
         {
-            
+            var x = new FormulaSimulator();
+            x.LoadFormulas("C:\\Users\\Тая\\source\\repos\\LordPelmeha\\Homework10\\Homework10\\MemorizingTheory.txt");
+            x.Training();
         }
     }
 }
